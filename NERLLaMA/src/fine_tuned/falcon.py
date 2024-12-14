6ffc63748192de9382f0af636f7dbbb5c1763b2c
Madhavi <xyzmadhavi@gmail.com>
2024-12-14 19:40:37 +0530

    Falcon model support

--- NERLLaMA/src/fine_tuned/falcon.py
+++ NERLLaMA/src/fine_tuned/falcon.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import wandb
 from tqdm import tqdm
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import transformers
 import torch
 
@@ -18,6 +18,7 @@ from NERLLaMA.src.schemas.DataStruct import create_train_test_instruct_datasets
 
 def generate(model, sources, generation_config):
     model_name = "tiiuae/falcon-7b-instruct"
+    # model_name = "vilsonrodrigues/falcon-7b-instruct-sharded"
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
@@ -39,36 +40,87 @@ def generate(model, sources, generation_config):
 
     # exit(0)
     # test_dataset = dataset
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=False,
+        load_in_8bit=True,
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_quant_type="fp4",
+        bnb_4bit_use_double_quant=True,
+        llm_int8_enable_fp32_cpu_offload=True
+    )
+    model = AutoModelForCausalLM.from_pretrained(model_name,
+                                             device_map="auto",
+                                             #device_map = device_map,
+    # quantization_config=quantization_config, 
+    trust_remote_code=True)
+
+    generation_config = model.generation_config
+
+    generation_config.temperature = 0
+    generation_config.num_return_sequences = 1
+    generation_config.max_new_tokens = 256
+    generation_config.use_cache=True
+    generation_config.eos_token_id=tokenizer.eos_token_id
+    generation_config.pad_token_id=tokenizer.eos_token_id
+
+    # pipeline = transformers.pipeline(
+    #             "text-generation",
+    #             model=model,
+    #             tokenizer=tokenizer,
+    #             #torch_dtype=torch.bfloat16,
+    #             trust_remote_code=True,
+    #             device_map="auto",
+    #             #device_map=device_map,
+    #             max_length=200,
+    #             use_cache=True,
+    #             num_return_sequences=1,
+    #             eos_token_id=tokenizer.eos_token_id,
+    #             pad_token_id=tokenizer.eos_token_id,
+    #         )
+    # generation_output = pipeline(
+    #             "Create a list of four important things a pilot should take note of"
+    #         )
+    # for s in generation_output:
+    #     print("Response: ", s)
+    
+    # return
+    
     for instruction in tqdm(test_dataset):
         target_list.append(instruction["raw_entities"])
         instruction_ids.append(instruction["id"])
-        sources.append(instruction["source"])
+        sources.append(instruction["input"])
 
     target_list = list(batch(target_list, n=1))
     instruction_ids = list(batch(instruction_ids, n=1))
     sources = list(batch(sources, n=1))
     for source in tqdm(sources):
-        with torch.no_grad():
-            torch.cuda.empty_cache()
-            gc.collect()
-            pipeline = transformers.pipeline(
-                "text-generation",
-                model=model,
-                tokenizer=tokenizer,
-                torch_dtype=torch.bfloat16,
-                trust_remote_code=True,
-                device_map="auto",
+        print(source)
+        prompt = """
+            Human: Extract all Chemical entities from the given text. Text The N-containing portions of milk can be divided into three broad fractions, including casein nitrogen (CN), whey protein nitrogen (WPN), and non protein nitrogen (NPN).
+            AI:
+        """
+        inputids = tokenizer(prompt, return_tensors='pt').input_ids
+        inputids = inputids.to(model.device)
+
+        with torch.inference_mode():
+            outputs = model.generate(
+                input_ids = inputids,
+                generation_config = generation_config,
             )
-            generation_output = pipeline(
-                source,
-                max_length=200,
-                do_sample=True,
-                top_k=10,
-                num_return_sequences=1,
-                eos_token_id=tokenizer.eos_token_id,
-            )
-        for s in generation_output:
-            print("Response: ", s)
+
+        response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        print(response)
+        # with torch.no_grad():
+        #     torch.cuda.empty_cache()
+        #     gc.collect()
+            
+        #     # generation_output = pipeline(
+        #     #     "Create a list of four important things a pilot should take note of"
+        #     # )
+
+            
+        # for s in generation_output:
+        #     print("Response: ", s)
 
 
 def run(
